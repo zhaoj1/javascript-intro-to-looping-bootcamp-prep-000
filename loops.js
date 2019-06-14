986ca4ceafa52e262a8950bdacbee5a02c5e9087
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-06-14 18:53:59 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function forLoop(array){
   for(var i=0;i<25;i++){
     if(1){
-      console.log("I am 1 strange loop.")
+      return `I am ${i} strange loop.`
     }
     else{
       return `I am ${i} strange loops.`
