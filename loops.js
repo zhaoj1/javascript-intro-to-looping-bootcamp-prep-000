5d819201e61c291c88e1e1994be4a5684123c3cf
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-06-14 19:28:25 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -18,10 +18,16 @@ function whileLoop(n){
   return "done"
 }
 
+var i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
 
 function doWhileLoop(num){
   do{
     console.log("I run once regardless");
   }
-    while(incrementVariable)
+    while(incrementVariable())
 }
